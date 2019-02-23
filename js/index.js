2f8d57b7c2b0b8ca73894692997799f7eaf5bee4
Scott Baker <sbaker1505@gmail.com>
2019-02-23 12:32:55 -0800

    photo file rename and styling updates

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 
 let main = document.getElementById('main')
 main.innerHTML = Home
+
+const loadPage = (element) => {main.innerHTML = element}
+
+
+// for (const link of links) {
+//   link.addEventListener('click', async (event) => {
+//     event.preventDefault()
+//     try {
+//       const module = await import (`/${link.dataset.entryModule}.mjs`)
+//       module.loadPageInto(main)
+//     } catch (error) {
+//       main.textContent = error.message
+//     }
+//   })
+// }
