3835daf01efe475dc191fcf31570a3e4f6f76051
Scott Baker <sbaker1505@gmail.com>
2019-02-23 19:34:57 -0800

    refactor sass files

--- js/home.mjs
+++ js/home.mjs
@@ -13,14 +13,4 @@ const Home = `
         <h1>What we do!</h1>
         <p>Fusce volutpat arcu in pellentesque cursus. Proin et hendrerit eros, in varius felis. Nullam elementum varius malesuada. Cras libero neque, eleifend vitae metus id, blandit commodo nisi. Duis ultricies sapien vitae sem placerat, et auctor nunc suscipit. Cras consectetur bibendum elit, sed molestie magna convallis imperdiet. Donec accumsan mollis sapien at ultrices. Donec congue efficitur velit, at gravida dolor luctus eu. In non mauris elementum, hendrerit dolor et, scelerisque nisi. Phasellus eleifend, sem in placerat sodales, enim eros rutrum tortor, quis posuere diam massa in augue. Cras lobortis dapibus lacus nec ornare. Curabitur at erat dictum, scelerisque arcu sed, tristique augue. Donec quis hendrerit ligula, sed semper elit. Mauris quis mollis sem. Donec tristique ante eget enim volutpat, vitae dignissim odio egestas.</p>
       </div>
-    </article>
-
-    <article class="products">
-      <hr>
-      <h2>Productos</h2>
-      <div class="products-list">
-        <div><h3>Microgreens</h3></div>
-        <div><h3>Flores Comestibles</h3></div>
-        <div><h3>Vegetales Extraordinarios</h3></div>
-      </div>
     </article>`
--- js/products.mjs
+++ js/products.mjs
@@ -1,1 +1,10 @@
-const Products = `<div>hello world</div>`
+const Products = `
+  <article class="products">
+  <hr>
+  <h2>Productos</h2>
+  <div class="products-list">
+    <div><h3>Microgreens</h3></div>
+    <div><h3>Flores Comestibles</h3></div>
+    <div><h3>Vegetales Extraordinarios</h3></div>
+  </div>
+</article>`
