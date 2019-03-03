cd0df031b891de02a43320b4dee0191efb36dd1c
John Torres <john.torres.hurtado@gmail.com>
2019-03-02 20:39:27 -0800

    Changes in footer and tweak animation

--- js/index.js
+++ js/index.js
@@ -1,24 +1,7 @@
 
-// let main = document.getElementById('main')
-// main.innerHTML = Home
-//
-// const loadPage = (element) => {main.innerHTML = element}
-
-
-// for (const link of links) {
-//   link.addEventListener('click', async (event) => {
-//     event.preventDefault()
-//     try {
-//       const module = await import (`/${link.dataset.entryModule}.mjs`)
-//       module.loadPageInto(main)
-//     } catch (error) {
-//       main.textContent = error.message
-//     }
-//   })
-// }
 window.addEventListener('scroll', function() {
   let logo = document.querySelector('.logo')
-  pageYOffset >= 420 ? 
+  pageYOffset >= 20 ?
     logo.classList.add   ("scroll-logo"):
     logo.classList.remove("scroll-logo")
 });
