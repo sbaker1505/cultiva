4e776a8e0887094a2b505c56caf056030024ad23
Scott Baker <sbaker1505@gmail.com>
2019-03-09 12:19:30 -0800

    image relocation and catalog init setup

---
+++ js/catalog.js
@@ -0,0 +1,31 @@
+const catalog = {
+  microgreens: [
+    {
+      name: {     en: 'English name (Acedera)',
+                  es: 'Acedera'},
+      id: 0000 0001,
+      image: ['microgreens/acedera.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor suave con un toque ácido.'},
+        look: {   en: 'english text here',
+                  es: 'Brillantes hojas verdes con nervaduras rojas y tallos rojos.'},
+      }
+    },{
+      name: {     en: 'English name (Acelga Arco Iris)',
+                  es: 'Acelga Arco Iris'},
+      id: 0000 0002,
+      image: [],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor Suave'},
+        look: {   en: 'english text here',
+                  es: 'Hojas de color verde amarillento, con tallos de deferentes colores, rojo, rosado, amarillo, fucsia y anaranjado.'},
+      }
+    },
+  ],
+  flowers: [],
+  vegetables: []
+}
