e55022b02441213f38318274947a6a2d089d4877
Scott Baker <sbaker1505@gmail.com>
2019-03-09 15:04:25 -0800

    product page mapping

--- js/catalog.js
+++ js/catalog.js
@@ -3,8 +3,8 @@ const catalog = {
     {
       name: {     en: 'English name (Acedera)',
                   es: 'Acedera'},
-      id: 0000 0001,
-      image: ['microgreens/acedera.jpg'],
+      id: 0001,
+      image: ['acedera.jpg'],
       keywords: [],
       discription: {
         taste: {  en: 'english text here',
@@ -15,14 +15,62 @@ const catalog = {
     },{
       name: {     en: 'English name (Acelga Arco Iris)',
                   es: 'Acelga Arco Iris'},
-      id: 0000 0002,
-      image: [],
+      id: 0002,
+      image: ['acelga_arco_iris.jpg'],
       keywords: [],
       discription: {
         taste: {  en: 'english text here',
                   es: 'Sabor Suave'},
         look: {   en: 'english text here',
                   es: 'Hojas de color verde amarillento, con tallos de deferentes colores, rojo, rosado, amarillo, fucsia y anaranjado.'},
+      }
+    },{
+      name: {     en: 'English name (Albahaca Limón)',
+                  es: 'Albahaca Limón'},
+      id: 0003,
+      image: ['albahaca_limon.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor fuerte, dulce y armoa cítrico.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas lanceoladas de color verde claro y tallos verde pálido.'},
+      }
+    },{
+      name: {     en: 'English name (Albahaca Morada)',
+                  es: 'Albahaca Morada'},
+      id: 0004,
+      image: ['albahaca_morada.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor dulce intenso aromática.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas moradas veteadas y tallo rojo.'},
+      }
+    },{
+      name: {     en: 'English name (Amaranto Rojo)',
+                  es: 'Amaranto Rojo'},
+      id: 0005,
+      image: ['amaranto.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor dulce y almendrado.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas en láminas muy finas ovales de colores rosa, rojizo morado y tallo rojo.'},
+      }
+    },{
+      name: {     en: 'English name (Anís)',
+                  es: 'Anís'},
+      id: 0006,
+      image: ['anis.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor dulce con un fuerte aroma típico de anís.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas redondas de color café verdoso.'},
       }
     },
   ],
--- js/index.js
+++ js/index.js
@@ -5,3 +5,17 @@ window.addEventListener('scroll', function() {
     logo.classList.add   ("scroll-logo"):
     logo.classList.remove("scroll-logo")
 });
+
+let lang = 'es';
+
+function products(type) {
+  const list = catalog[type];
+  const ProductList = document.getElementById('productList');
+  ProductList.innerHTML = list.map(item =>
+    `<div class="product-item">
+      <img src="../img/${type}/${item.image[0]}" alt=""/>
+      <h3>${item.name[lang]}</h3>
+      <p class="taste">${item.discription.taste[lang]}</p>
+      <p class="look">${item.discription.look[lang]}</p>
+    </div>`).join('')
+}
