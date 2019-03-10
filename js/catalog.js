093c146fc8ffc03a6c6cd5b5d5eef3fe0f791f9f
Scott Baker <sbaker1505@gmail.com>
2019-03-10 13:27:15 -0700

    catalog update - microgreens

--- js/catalog.js
+++ js/catalog.js
@@ -72,6 +72,246 @@ const catalog = {
         look: {   en: 'english text here',
                   es: 'Hojas redondas de color café verdoso.'},
       }
+    },{
+      name: {     en: 'English name (Apio)',
+                  es: 'Apio'},
+      id: '0007',
+      image: ['apio.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor intenso amargo y fuerte aroma muy típico del apio conside rada como una planta armoática y medicinal.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas y tallos de color verde.'},
+      }
+    },{
+      name: {     en: 'English name (Bálsamo Limón)',
+                  es: 'Bálsamo Limón'},
+      id: '0008',
+      image: ['balsamo_limon.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor dulce con un fuerte aroma.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas redondas rugosas verde obscuro.'},
+      }
+    },{
+      name: {     en: 'English name (Brócoli)',
+                  es: 'Brócoli'},
+      id: '0009',
+      image: ['brocoli.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor a acre y a nuez.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas y tallos verde claro.'},
+      }
+    },{
+      name: {     en: 'English name (Col)',
+                  es: 'Col'},
+      id: '0010',
+      image: ['col.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor sutil profundo y terroso.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas medianas verdes con tallos verde.'},
+      }
+    },{
+      name: {     en: 'English name (Col Morada)',
+                  es: 'Col Morada'},
+      id: '0011',
+      image: ['col_morada.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor sutil profundo y terroso.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas medianas verde claras y verde oscuras con tallos morados.'},
+      }
+    },{
+      name: {     en: 'English name (Crisantemo)',
+                  es: 'Crisantemo'},
+      id: '0012',
+      image: ['crisantemo.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor fresco picante y aromático.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas dentadas muy ornamen tales de color verde y tallos verde claro.'},
+      }
+    },{
+      name: {     en: 'English name (Culantro)',
+                  es: 'Culantro'},
+      id: '0013',
+      image: ['culantro.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor cítrico fuerte y pungente.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas verdes ornamentales, dentadas y tallo verde amarillo.'},
+      }
+    },{
+      name: {     en: 'English name (Kale (Acelga Rusa))',
+                  es: 'Kale (Acelga Rusa)'},
+      id: '0014',
+      image: ['kale.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor suave y un poco amargo.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas delineadas de color azul verdoso y tallos de color lila.'},
+      }
+    },{
+      name: {     en: 'English name (Magenta)',
+                  es: 'Magenta'},
+      id: '0015',
+      image: ['magenta.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor suave delicado.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas de color verdoso con un centro de color magenta y tallos de color verde rojiso.'},
+      }
+    },{
+      name: {     en: 'English name (Mostaza Espinaca (Tatsoi))',
+                  es: 'Mostaza Espinaca (Tatsoi)'},
+      id: '0016',
+      image: ['mostaza_espinaca.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor a mostaza tierna un poco picante.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas pequeñas de color verde muy jugosas.'},
+      }
+    },{
+      name: {     en: 'English name (Mostaza Gigante)',
+                  es: 'Mostaza Gigante'},
+      id: '0017',
+      image: ['mostaza_gigante.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor picante y concentrado.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas verdes difuminadas de vino tinto, tiernas y picantes y tallos pálidos.'},
+      }
+    },{
+      name: {     en: 'English name (Mostaza Japonesa)',
+                  es: 'Mostaza Japonesa'},
+      id: '0018',
+      image: ['mostaza_japonesa.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor picante leve.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas dentadas y lanceoladas tiernas de color verde y tallo lila.'},
+      }
+    },{
+      name: {     en: 'English name (Mostaza Japonesa Morada)',
+                  es: 'Mostaza Japonesa Morada'},
+      id: '0019',
+      image: ['mostaza_japonesa_morada.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor picante leve.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas y tallos morados.'},
+      }
+    },{
+      name: {     en: 'English name (Pac Choi)',
+                  es: 'Pac Choi'},
+      id: '0020',
+      image: ['pac_choi.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor amargo con un ligero toque dulce.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas verde oscurdo, nervaduras moradas y tallos de color verde claro.'},
+      }
+    },{
+      name: {     en: 'English name (Rábano)',
+                  es: 'Rábano'},
+      id: '0021',
+      image: ['rabano.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor intenso y concentrado.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas y tallos de color verde jugosas.'},
+      }
+    },{
+      name: {     en: 'English name (Remolacha/Remolacha Roja)',
+                  es: 'Remolacha/Remolacha Roja'},
+      id: '0022',
+      image: ['remolacha.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor dulce amargo y un toque picante.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas verdes con nervaduras rojas y tallos fucsia. Hojas tallos rojo oscuro.'},
+      }
+    },{
+      name: {     en: 'English name (Rúcula)',
+                  es: 'Rúcula'},
+      id: '0023',
+      image: ['rucula.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor fuerte, picante y amargo.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas verdes redondeadas y tallo verde claro.'},
+      }
+    },{
+      name: {     en: 'English name (Verdolaga)',
+                  es: 'Verdolaga'},
+      id: '0024',
+      image: ['verdolaga.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor suave.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas de color claro, carnosas y jugosas.'},
+      }
+    },{
+      name: {     en: 'English name (Wasabi)',
+                  es: 'Wasabi'},
+      id: '0025',
+      image: ['wasabi.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor intenso picante.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas de color verde oscuro, al igual que los tallos.'},
+      }
+    },{
+      name: {     en: 'English name (Zanahoria)',
+                  es: 'Zanahoria'},
+      id: '0026',
+      image: ['zanahoria.jpg'],
+      keywords: [],
+      discription: {
+        taste: {  en: 'english text here',
+                  es: 'Sabor típico de zanahoria.'},
+        look: {   en: 'english text here',
+                  es: 'Hojas de dos tipos lanceoladas y aserrads de color verde, tallos delgados y verdes.'},
+      }
     },
   ],
   flowers: [],
